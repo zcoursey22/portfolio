0e350634051bdc65345a9dfa5761a703d4ac0025
Zach Coursey <zcoursey22@gmail.com>
2018-08-22 13:52:11 -0700

    Initial set up

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1 className="App-title">ZACH COURSEY</h1>
+        </header>
+        <p>Welcome to my portfolio!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
