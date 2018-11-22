e747b5a4a4680eeb1dafc5fe0410b00308f2015a
Zach Coursey <zcoursey22@gmail.com>
2018-11-21 22:17:20 -0500

    Adds form validation and changes style of form

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './styles/Contact.css';
 import axios from 'axios';
+import validator from 'email-validator';
 
 class Contact extends Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class Contact extends Component {
       email: '',
       message: ''
     }
+    this.resetStyle = this.resetStyle.bind(this);
   }
 
   toggleForm() {
@@ -35,8 +37,58 @@ class Contact extends Component {
     });
   }
 
+  validateForm() {
+    const removeWhiteSpace = (str) => {
+      return str.split(' ').join('');
+    }
+
+    const validateName = () => {
+      let valid = true;
+      if (removeWhiteSpace(this.state.name) === '') {
+        valid = false;
+        document.querySelector("#nameInput").style.background = '#ffdfea';
+        document.querySelector("#nameInput").style.border = '2px solid #b66';
+        document.querySelector("#nameInput").placeholder = 'Enter your name';
+      }
+      return valid;
+    }
+
+    const validateEmail = () => {
+      let valid = true;
+      if (removeWhiteSpace(this.state.email) === '') {
+        valid = false;
+        document.querySelector("#emailInput").style.background = '#ffdfea';
+        document.querySelector("#emailInput").style.border = '2px solid #b66';
+        document.querySelector("#emailInput").placeholder = 'Enter your email address';
+      } else if (!validator.validate(this.state.email)) {
+        valid = false;
+        document.querySelector("#emailInput").value = '';
+        document.querySelector("#emailInput").style.background = '#ffdfea';
+        document.querySelector("#emailInput").style.border = '2px solid #b66';
+        document.querySelector("#emailInput").placeholder = 'Email address invalid';
+      }
+      return valid;
+    }
+
+    const validateMessage = () => {
+      let valid = true;
+      if (removeWhiteSpace(this.state.message) === '') {
+        valid = false;
+        document.querySelector("textarea").style.background = '#ffdfea';
+        document.querySelector("textarea").style.border = '2px solid #b66';
+        document.querySelector("textarea").placeholder = 'Please enter a message';
+      }
+      return valid;
+    }
+
+    let formValid = true;
+    if (validateName() === false) formValid = false;
+    if (validateEmail() === false) formValid = false;
+    if (validateMessage() === false) formValid = false;
+    if (formValid) this.submitForm();
+  }
+
   submitForm() {
-    // Validate form
     this.setState({
       submitted: true
     }, () => {
@@ -60,6 +112,26 @@ class Contact extends Component {
     });
     document.querySelectorAll("input").forEach(input => input.value = '');
     document.querySelector("textarea").value = '';
+    this.resetAllStyles();
+  }
+
+  resetStyle(e) {
+    e.target.style.background = '#444';
+    e.target.style.border = '2px solid #111';
+    e.target.placeholder = '';
+    if (e.target.id === 'emailInput') e.target.value = this.state.email;
+  }
+
+  resetAllStyles() {
+    document.querySelector("#nameInput").style.background = '#444';
+    document.querySelector("#nameInput").style.border = '2px solid #111';
+    document.querySelector("#nameInput").placeholder = '';
+    document.querySelector("#emailInput").style.background = '#444';
+    document.querySelector("#emailInput").style.border = '2px solid #111';
+    document.querySelector("#emailInput").placeholder = '';
+    document.querySelector("textarea").style.background = '#444';
+    document.querySelector("textarea").style.border = '2px solid #111';
+    document.querySelector("textarea").placeholder = '';
   }
 
   render() {
@@ -82,14 +154,14 @@ class Contact extends Component {
         <p style={{ display: this.state.buttonVisible ? 'none' : 'block' }} className={getParagraphStyle()}><span onClick={() => this.setState({ buttonVisible: true })} id="sendAnotherMessage">Want to send another message?</span></p>
         <div id="form" className={this.state.visible ? 'open-form' : 'close-form'}>
           <form>
-            <img src="images/x.png" onClick={this.toggleForm.bind(this)}/>
+            <img id="xButton" src="images/x.png" onClick={this.toggleForm.bind(this)}/>
             <label>NAME</label>
-            <input id="nameInput" onChange={(e) => this.setState({ name: e.target.value })}></input>
+            <input id="nameInput" onClick={(e) => this.resetStyle(e)} onChange={(e) => this.setState({ name: e.target.value })}></input>
             <label>EMAIL</label>
-            <input onChange={(e) => this.setState({ email: e.target.value })}></input>
+            <input id="emailInput" onClick={(e) => this.resetStyle(e)} onChange={(e) => this.setState({ email: e.target.value })}></input>
             <label>MESSAGE</label>
-            <textarea onChange={(e) => this.setState({ message: e.target.value })}></textarea>
-            <button type="button" onClick={this.submitForm.bind(this)}>SUBMIT</button>
+            <textarea onClick={(e) => this.resetStyle(e)} onChange={(e) => this.setState({ message: e.target.value })}></textarea>
+            <button type="button" onClick={this.validateForm.bind(this)}>SUBMIT</button>
           </form>
         </div>
       </div>
